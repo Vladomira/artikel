bd19576d7bd36137b994882121afc8238a09e9c4
Julia_Vladomira <juliavladomira@gmail.com>
2022-10-20 14:26:33 +0200

    some corrects

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <main>
                 <ActualArticles />
                 <ArtSection />
-                {/* <LastArticles /> */}
+                <LastArticles />
                 <PopularAuthors />
             </main>
         </>
