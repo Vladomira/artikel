e1fcde0e05741655a5264dc260bbe0531de3570d
Julia_Vladomira <juliavladomira@gmail.com>
2022-10-26 12:56:44 +0200

    fix

--- src/api/chapters-bottom-request.ts
+++ src/api/chapters-bottom-request.ts
@@ -7,5 +7,6 @@ export type BottomChapter = {
 export type BottomChaptersProps = { chapters: BottomChapter[] };
 
 export function fetchBottomChapters(): AxiosPromise<BottomChaptersProps> {
-    return axios.get(`${process.env.API_HOST}/bottom-chapters`);
+    // return axios.get(`${process.env.API_HOST}/bottom-chapters`);
+    return axios.get("http://localhost:3000/api/bottom-chapters");
 }
